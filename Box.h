6e9bcf116efea21609f7e1bebd3ae1399760dd50
Vorlon <136607677+finchyyyy@users.noreply.github.com>
2025-12-21 20:57:22 +0000

    Moving to header + implementation

--- Box.h
+++ Box.h
@@ -1,39 +1,16 @@
+//Box.h
 #pragma once
+
 class Box {
 public:
-	unsigned int type = 0;		//make sure this is an int, it gets fucky when you make it a char
+	unsigned int type = 0;
 
 	int posX, posY;
 	int width, height;
 
-	virtual ~Box() {			//needed for dynamic_cast stuff
+	virtual ~Box() {
 
 	}
 };
 
-class Colbox : public Box {
-public:
-
-	Colbox() {
-		type = 1;
-	}
-};
-
-class Hurtbox : public Box {
-public:
-
-	bool invincible = false;
-
-	Hurtbox() {
-		type = 2;
-	}
-};
-
-class Hitbox : public Box {
-public:
-	//AtkData atkdata;
-
-	Hitbox() {
-		type = 3;
-	}
-};
+bool overlap(Box a, Box b);
