4dcf616583befc0b980d3ff7962c72bc409095e2
Vorlon <136607677+finchyyyy@users.noreply.github.com>
2026-06-22 09:31:14 +0100

    drawer_h: small changes
    removing pragma once
    god why do i use so much whitespace

--- Drawer.h
+++ Drawer.h
@@ -1,9 +1,16 @@
-#pragma once
+#ifndef DRAWER_H
+#define DRAWER_H
+
+//> these are reliant on SFML, do I want this?
+//^ time to make my own graphics library babyyyyyyyyy
 #include <SFML/Graphics.hpp>
 
+//> This header is basically just extensions for SFML to draw me-defined objects.
+//^ These are intended to be used inside main().
+//^ These are intended to be run once a frame for as long as I care about them.
 
 	//draws a point
-void draw(sf::RenderWindow& window, int x, int y) {
+inline void draw(sf::RenderWindow& window, int x, int y) {
 	sf::CircleShape circ(2.0f);
 	
 	circ.setPosition((float)x, (float)y);
@@ -11,34 +18,28 @@ void draw(sf::RenderWindow& window, int x, int y) {
 	circ.setFillColor(sf::Color::Red);
 
 	window.draw(circ);
-
 }
 
 	//draws a box
-void draw(sf::RenderWindow& window, Box box) {
+inline void draw(sf::RenderWindow& window, Box box) {
 
 	sf::RectangleShape rect;
 	sf::Color colour;
 
 	switch (box.type) {
-
 	case 1:
 		colour = sf::Color::Magenta;
 		break;
-
 	case 2:
 		colour = sf::Color::Blue;
 		break;
-
 	case 3:
 		colour = sf::Color::Red;
 		break;
-
 	default:
 		colour = sf::Color::Cyan;
 		break;
 	}
-
 
 	rect.setPosition(box.posX, box.posY);
 	rect.setSize(sf::Vector2f(box.width, box.height));
@@ -48,65 +49,52 @@ void draw(sf::RenderWindow& window, Box box) {
 	rect.setOutlineThickness(-2.0f);
 	
 	window.draw(rect);
-
 }
 
-void draw(sf::RenderWindow& window, Object object, bool extra_data = false) {
+inline void draw(sf::RenderWindow& window, Object object, bool extra_data = false) {
 
-	//draw sprite - add when sprites added
 	window.draw(object.sprite);
-
-	//draw boxes and position if requested
+	
 	if (extra_data) {
-
 		//boxes
-		for (int i = 0; i < object.box_count; i++) {
+		for (int i = 0; i < object.box_count; i++)
 			draw(window, object.boxes[i]);
-		}
-
-		//position
+		//reference position
 		draw(window, object.physics.posX, object.physics.posY);
-
 	}
-
 }
 
-
-void draw(sf::RenderWindow& window, DPad dpad, int x, int y) {
-
+	//draws a DPad
+inline void draw(sf::RenderWindow& window, DPad dpad, int x, int y) {
 	sf::CircleShape circ(15.0f);
+	
 	circ.setFillColor(sf::Color::Green);
 	circ.setPosition(x + dpad.x * 30, y + dpad.y * 30);
+	
 	window.draw(circ);
-
 }
 
-void draw(sf::RenderWindow& window, Button button, int x, int y) {
+	//draws a Button
+inline void draw(sf::RenderWindow& window, Button button, int x, int y) {
 
 	sf::CircleShape circ(8.0f);
 
-	//button
+	circ.setPosition(x, y);
 	switch (button.status) {
-
 	case Button::JUST_PRESSED:
 		circ.setFillColor(sf::Color::Red);
 		break;
-
 	case Button::JUST_RELEASED:
 		circ.setFillColor(sf::Color::Blue);
 		break;
-
 	case Button::NO_CHANGE:
 		circ.setFillColor(sf::Color::Transparent);
 		break;
-
 	default:
-		//shit is fucked yo
+		//> shit is fucked yo
 		circ.setFillColor(sf::Color::Green);
-
+		break;
 	}
-
-	circ.setPosition(x, y);
+	
 	window.draw(circ);
-
 }
