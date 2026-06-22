85afb310cd1c30236a8d077a217b65da831ffefa
Vorlon <136607677+finchyyyy@users.noreply.github.com>
2026-06-22 08:33:28 +0100

    physics_h: moving to header + imp
    cut all of the implementation stuff, moving it to own file

--- Physics.h
+++ Physics.h
@@ -1,22 +1,21 @@
-#pragma once
-#define FLOOR 0
-
-//_op are bitflags that can be used for anything
-
-//for applying forces
+#ifndef PHYSICS_H
+#define PHYSICS_H
+//> Physics objects can represent one of two things:
+//
+//> Actual objects: These are things that we want to simulate, and usually run update() every game tick
+//^ They are the intended target for apply_force()
+//^ in this form, only accX_op and accY_op are used as flags, to denote whether friction and gravity apply to them:
+#define FRICTION accX_op
+#define GRAVITY accY_op
+//
+//> Forces: These are forces we apply to actual objects (the Physics param in apply_force())
+//^ THEY ARE NOT INTENDED TO BE USED BY update()
+//^ in this form, all _op flags have use, specifically to decide whether the base variable is to be added to or write over the original:
 #define ADD false
 #define REPLACE true
 
-//for updating objects
-#define FRICTION accX_op
-#define GRAVITY accY_op
-
-#define FRICTION_VAL -1
-#define GRAVITY_VAL -1
-
 class Physics {
 public:
-
 	int posX, posY; bool posX_op, posY_op;		//position
 	int velX, velY; bool velX_op, velY_op;		//velocity
 	int accX, accY; bool accX_op, accY_op;		//acceleration
@@ -26,52 +25,4 @@ public:
 	void update();
 
 };
-
-
-void Physics::apply_force(Physics force, int x_dir = 1, int y_dir = 1) {
-
-	if (force.posX_op == ADD) posX += force.posX * x_dir;
-	else					  posX = force.posX;
-
-	if (force.posY_op == ADD) posY += force.posY * y_dir;
-	else					  posY = force.posY;
-
-	if (force.velX_op == ADD) velX += force.velX * x_dir;
-	else					  velX = force.velX * x_dir;
-
-	if (force.velY_op == ADD) velY += force.velY * y_dir;
-	else					  velY = force.velY * y_dir;
-
-	if (force.accX_op == ADD) accX += force.accX * x_dir;
-	else					  accX = force.accX * x_dir;
-
-	if (force.accY_op == ADD) accY += force.accY * y_dir;
-	else					  accY = force.accY * y_dir;
-
-}
-
-void Physics::update() {
-
-	velX += accX;
-	velY += accY;
-
-	if (FRICTION) {
-
-		if (velX)
-
-			velX += FRICTION_VAL;
-
-	}
-	if (GRAVITY) {
-
-		velY += GRAVITY_VAL;	//acts down
-
-		if (posY == FLOOR) velY = 0;
-	}
-
-	posX += velX;
-	posY += velY;
-
-	if (posY < FLOOR) posY = 0;
-
-}
+#endif
